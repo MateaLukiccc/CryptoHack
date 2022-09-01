e873f3f23d57216eea2440cf062bb855993971bb
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2022-09-01 18:48:01 +0200

    Added Math folder and GCD solution

---
+++ Mathematics/Greatest_Common_Divisor.py
@@ -0,0 +1,6 @@
+#There are many tools to calculate the GCD of two integers, but for this task we recommend looking up Euclid's Algorithm.
+#Now calculate gcd(a,b) for a = 66528, b = 52920 
+
+import math
+
+print(math.gcd(66528,52920))
