603e336ce33b0303465af83b630f2f2249d70752
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2022-09-17 11:29:14 +0200

    Changed solution from ECB CBC WTF

--- Symmetric Ciphers/Block Ciphers/ECB_CBC_WTF.py
+++ Symmetric Ciphers/Block Ciphers/ECB_CBC_WTF.py
@@ -6,42 +6,44 @@ We can encrypt in CBC and decrypt in EBC so we need to undestand different modes
 
 So since ct is 96 hex digits that is 48 bytes and we have 3 16 byte blocks B1 B2 B3
 CBC encryption:
-C1=E(IV ^ P1)
-C2=E(C1 ^ P2)
-C3=E(C2 ^ P3)
+C1=IV
+C2=E(IV ^ P1)
+C3=E(C1 ^ P2)=E(E(IV ^ P1) ^ P2)
+
+so ciphertext=IV+E(IV ^ P1)+E(E(IV ^ P1) ^ P2)
 
 Since we have decryption option with EBC it stands that:
 
-O1=D(C1)=D(E(IV ^ B1))=IV ^ P1
-O2=C1 ^ P2
-O3=C2 ^ P3
+O1=D(IV)
+O2=IV ^ P1
+O3=E(IV ^ P1) ^ P2
 
-so we can look at C1 as IV and make it like we know IV so our task becomes
 
-ciphertext=IV+C1+C2 and since we can decrypt C1 and C2 we have O1=IV ^ P1 and O2= C1 ^ P2 =>P1=IV ^ O1 and we can get P2 so we have the flag
+from C1 ^ O2 we can get P1 and then P2
 '''
 
+
+ct="2f073a622d39e70119b32df22c9abfdf39cdef6e511b2e390e17c052bd61566fb124db243de4e629f300e8675134f3fb"
+
+pt="a7cf3248e76adf7ab26339503a485ca74c75431259569c327ad172c759f9d4ea66f9995e607f71083948e1739c407712"
+
+from pwn import xor
+
+iv=ct[:32]
+db1=xor(bytes.fromhex(iv),bytes.fromhex(pt[32:64]))
+db2=xor(bytes.fromhex(ct[32:64]),bytes.fromhex(pt[64:]))
+
+print(db1,db2,sep="")
+
+
+#different approach
+'''
 def xor(a,b):
     a = bytes.fromhex(a)
     b = bytes.fromhex(b)
     return bytes(i ^ j for i,j in zip(a,b))
+'''
 
-
-ct="ce801a6ee42f65686a0b5b406e70fb7d26afc3a4146782049bc453d577d1effa5650ee2fca019041faef892085da7e1a"
-
-
-import textwrap
-iv=textwrap.wrap(ct, len(ct)//3)[0]
-ciphertext_block1=textwrap.wrap(ct, len(ct)//3)[1]
-ciphertext_block2=textwrap.wrap(ct, len(ct)//3)[2] 
-
-
-decrypted_block2="799bb5942503dd35ac9b72f456f0ce87"
-plaintext_block2 = xor(decrypted_block2,ciphertext_block1)
-
-decrypted_block1="adf2631e90401e5b096904751b139048"
-plaintext_block1 = xor(decrypted_block1,iv)
-
-print(plaintext_block1+plaintext_block2)
-
-
+#another way to split text 
+#import textwrap
+#iv=textwrap.wrap(ct, len(ct)//3)[0]
